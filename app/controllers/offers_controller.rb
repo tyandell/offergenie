6ec446af987a127456506fbd3c7ee373e9dafd43
Todd Yandell <tyandell@gmail.com>
2023-09-05 04:26:40 -0500

    Add fallback offer order

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,6 @@ class OffersController < ApplicationController
   before_action :require_user
 
   def index
-    @pagy, @offers = pagy(Offer.available.recommended_for(current_user), items: 12)
+    @pagy, @offers = pagy(Offer.available.recommended_for(current_user).order(created_at: :desc), items: 12)
   end
 end
